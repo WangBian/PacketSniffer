24792108acab99bd78af88b85290d46086a503f4
Wang Bian <wang.bian.37@gmail.com>
2015-11-29 11:20:24 -0500

    tcpdump and output display implemeted. -- George and Wang

--- app/src/main/java/com/example/wang/packetsniffer/MainActivity.java
+++ app/src/main/java/com/example/wang/packetsniffer/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.wang.packetsniffer;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +49,49 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    Process process;
+
     public void startButtonOnClick(View view){
         ((TextView)findViewById(R.id.progressView)).setText("IN PROGRESS");
+        try {
+            process = Runtime.getRuntime().exec(new String[]{"su", "-c", "chmod 777 /sdcard/programapp/tcpdump"});
+            process = Runtime.getRuntime().exec(new String[]{"su", "-c", "tcpdump -c 100 -l -i eth0 > /mnt/sdcard/programapp/output"});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void stopButtonOnClick(View view) {
         ((TextView) findViewById(R.id.progressView)).setText("COMPLETED");
+        process.destroy();
+        displayOutput();
     }
 
+    public void displayOutput()
+    {
+        StringBuilder text = new StringBuilder();
+        try {
+
+            FileInputStream fis = new FileInputStream("/mnt/sdcard/programapp/output");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+            String line;
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+        }
+        catch (FileNotFoundException e) {
+            Toast.makeText(getApplicationContext(), "File not found!", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "Error reading file!", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+        TextView output=(TextView) findViewById(R.id.trafficView);
+        output.setMovementMethod(new ScrollingMovementMethod());
+        output.setText(text);
+    }
 }
